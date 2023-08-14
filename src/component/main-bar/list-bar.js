d570be0d5408ff849d0231126c07d05fbbf0a871
chaewon02~ <glgl246@naver.com>
2023-08-15 00:03:04 +0900

    메인 상단바 반응형 웹 구현

---
+++ src/component/main-bar/list-bar.js
@@ -0,0 +1,20 @@
+import './list-bar.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
+
+
+function ListBar() {
+  return (
+    <ul className="navbar__menu">
+      <li className="nav-item"><Link to="/"><a>이용안내</a></Link></li>
+      <li className="nav-item"><Link to="/"><a>고객센터</a></Link></li>
+      <li className="nav-item"><Link to="/login"><a>로그인</a></Link></li>
+      <li className="nav-item"><Link to="/signup"><a>회원가입</a></Link></li>
+    </ul>
+   
+  );
+}
+
+export default ListBar;
--- src/main.js
+++ src/main.js
@@ -5,36 +5,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
 import Home from './homepage.js'
+import ListBar from './component/main-bar/list-bar';
+import BoxBar from './component/main-bar/box-bar';
 
 function Main() {
   return (
     <div>
-        <nav className="navbar">
-            <ul className="navbar__logo">
-              <Link to="/">
-                  <img src="img/logo.png" />
-              </Link>
-            </ul>
+      <nav className="navbar" id='mb'>
+        <div className='g'>
+          <ul className="navbar__logo">
+            <Link to="/">
+                <img src="img/logo.png" />
+            </Link>
+          </ul>
 
-            <ul className="navbar__search">
-              <Link to="/search">
-                <div className="navbar__search-input">
-                  <form className="d-flex" role="search">
-                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-                    <button className="btn btn-outline-success" type="submit">가고싶은 공공시설을 검색하세요 ! </button>
-                  </form>
-                </div>
-              </Link>
-            </ul>
-            
-            <ul className="navbar__menu">
-            <li className="nav-item"><Link to="/"><a>이용안내</a></Link></li>
-              <li className="nav-item"><Link to="/"><a>고객센터</a></Link></li>
-              <li className="nav-item"><Link to="/login"><a>로그인</a></Link></li>
-              <li className="nav-item"><Link to="/signup"><a>회원가입</a></Link></li>
-            </ul>
+          <ul className="navbar__search">
+            <Link to="/search">
+              <div className="navbar__search-input">
+                <form className="d-flex" role="search">
+                  <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                  <button className="btn btn-outline-success" type="submit">가고싶은 공공시설을 검색하세요 ! </button>
+                </form>
+              </div>
+            </Link>
+          </ul>
+        </div>
+          <ListBar className="list"/>
+          <BoxBar className="box"/>
         </nav>
         <Home/>
 
