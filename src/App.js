1769729892e2a1dcec03321d140858eafe3d9841
chaewon02~ <glgl246@naver.com>
2023-08-14 19:57:19 +0900

    폴더 정리/라우팅/내 코드 복사

--- src/App.js
+++ src/App.js
@@ -1,62 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import './App.scss';
+import { Route, Routes, BrowserRouter} from 'react-router-dom';
+import Locations from './pages/locaions';
+import Main from './main';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Search from './search.js'
-import Home from './homepage.js'
-import Login from './login.js'
-import Signup from './signup.js'
+import Login from './login.js';
+import Signup from './signup.js';
+import Search from './search.js';
+import Home from './homepage';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <nav className="navbar">
-            <ul className="navbar__logo">
-              <Link to="/Home">
-                  <img src="img/logo.png" />
-              </Link>
-            </ul>
-
-            <ul className="navbar__search">
-              <Link to="/search">
-                <div className="navbar__search-input">
-                  <form className="d-flex" role="search">
-                    <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
-                    <button className="btn btn-outline-success" type="submit">가고싶은 공공시설을 검색하세요 ! </button>
-                  </form>
-                </div>
-              </Link>
-            </ul>
-            
-            <ul className="navbar__menu">
-            <li className="nav-item"><Link to="/"><a>이용안내</a></Link></li>
-              <li className="nav-item"><Link to="/"><a>고객센터</a></Link></li>
-              <li className="nav-item"><Link to="/login"><a>로그인</a></Link></li>
-              <li className="nav-item"><Link to="/signup"><a>회원가입</a></Link></li>
-            </ul>
-        </nav>
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
-
-      
-
-
-    <br></br>
-    <br></br>
-      <div id="footer">
-        <p> 서대문구 불타는키보드 | 공공시설 예약서비스</p>
+    <div id='container'>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Main/>}/>
+            <Route path="/p/*" element={<Locations/>} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+          </Routes>
       </div>
     </div>
   );
