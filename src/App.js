017240e04d668e929fff9f7f974a7d55a398f941
dbogym <ko07073@gmail.com>
2023-08-18 22:22:46 +0900

    머지

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React from "react";
-// import './App.scss';
-import { Route, Routes, BrowserRouter} from 'react-router-dom';
-import Locations from './pages/locaions';
-import Main from './main';
-import 'bootstrap/dist/css/bootstrap.css';
-import Login from './login.js';
-import Signup from './signup.js';
-import Search from './search.js';
-import Home from './homepage';
-import MyPage from './pages/MyPage/mypage';
-import GymIconPage from './pages/IconPage/Gym.js';     // 변경된 부분
-import SpaceIconPage from './pages/IconPage/Space.js'; // 변경된 부분
-import CultureIconPage from './pages/IconPage/Culture.js'; // 변경된 부분
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Locations from "./pages/locaions";
+import Main from "./main";
+import "bootstrap/dist/css/bootstrap.css";
+import Login from "./login.js";
+import Signup from "./signup.js";
+import Home from "./homepage";
 
+import GymIconPage from "./pages/IconPage/Gym.js";
+import SpaceIconPage from "./pages/IconPage/Space.js";
+import CultureIconPage from "./pages/IconPage/Culture.js";
+
+import MyPage from "./pages/MyPage/mypage";
+import MyInfo from "./pages/MyPage/MyInfo";
+import MyReview from "./pages/MyPage/myreview";
+import FacilityDetail from "./component/facility/FacilityDetail";
+import SearchComponent from "./component/search/SearchComponent";
+import InformationUse from "./pages/InformationUse/InformationUse.js";
 
 function App() {
   return (
-    <div id='container'>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Main/>}/>
-            <Route path="/p/*" element={<Locations/>} />
-            <Route path='/search' element={<Search />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<Signup />} />
-            <Route path='/mypage' element={<MyPage />} />
-            <Route path="/gym" element={<GymIconPage />} />     // 변경된 부분
-            <Route path="/space" element={<SpaceIconPage />} /> // 변경된 부분
-            <Route path="/culture" element={<CultureIconPage />} /> // 변경된 부분
-          </Routes>
-
+    <div id="container">
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route path="/p/*" element={<Locations />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/gym" element={<GymIconPage />} />
+          <Route path="/space" element={<SpaceIconPage />} />
+          <Route path="/culture" element={<CultureIconPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/myinfo" element={<MyInfo />} />
+          <Route path="/myreview" element={<MyReview />} />
+          <Route path="/facility/:id" element={<FacilityDetail />} />
+          <Route path="/searchFacility" element={<SearchComponent />} />
+          <Route path="/n=InformationUse" element={<InformationUse />} />
+        </Routes>
       </div>
     </div>
   );
--- src/pages/MyPage/mypage.js
+++ src/pages/MyPage/mypage.js
@@ -1,55 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
-import OffcanvasExample from '../../component/bar/bar';
-import './mypage.scss';
-import {Link} from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import OffcanvasExample from "../../component/bar/bar";
+import "./mypage.scss";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import ModalBulb from "../../component/bulb/modalBulb";
 
 const Container = styled.div`
-//   height: 100vh;
+  //   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 60vw;
-    height: 200vw ;
-    padding: 1rem;
-    margin: 0 auto;
+  height: 200vw;
+  padding: 1rem;
+  margin: 0 auto;
 `;
 const MyDiv = styled.div`
-    height: 100%;
-    width: 100%;
-    // border: 2px #808080 solid;
-    border-radius: 30px;
-    padding: 60px;
-    padding-top : 30px;
+  height: 100%;
+  width: 100%;
+  // border: 2px #808080 solid;
+  border-radius: 30px;
+  padding: 60px;
+  padding-top: 30px;
 `;
 function MyPage() {
-  return(
-    <>  
-        <div id='searchbar'>
-            <OffcanvasExample />
-        </div>
-        <Container>
-            <MyDiv>
-              <div>
-                <h1>[마이 페이지]</h1>
-                <div>
-                  <div class="b_container">
-                    <Link to='/myinfo'>
-                        <button class="btn-1">내 정보</button>
-                    </Link>
-                    <Link to='/myreview'>
-                        <button class="btn-2">내가 쓴 리뷰 보러가기</button>
-                    </Link>
-                  </div>
-                </div>
+  const bname = "마이페이지";
+  const ex =
+    "마이 페이지 입니다.\n해당 페이지에서는 회원님의 회원 정보와 이전에 작성했던 후기 리스트를 확인하실 수 있습니다.";
+  return (
+    <>
+      <div id="searchbar">
+        <OffcanvasExample />
+      </div>
+      <ModalBulb bname={bname} ex={ex} />
+      <Container>
+        <MyDiv>
+          <div>
+            <h1>[마이 페이지]</h1>
+            <div>
+              <div class="b_container">
+                <Link to="/myinfo">
+                  <button class="btn-1">내 정보</button>
+                </Link>
+                <Link to="/myreview">
+                  <button class="btn-2">내가 쓴 리뷰 보러가기</button>
+                </Link>
               </div>
-            </MyDiv>
-        </Container>
-      
-      <Routes> 
-        <Route path='/mypage/myinfo' element={<MyInfo/>} />
-        <Route path='/mypage/myreview' element={<MyReview reviews={reviews}/>}/>
-      </Routes>
+            </div>
+          </div>
+        </MyDiv>
+      </Container>
     </>
   );
 }
